cd9a7afa6a9db3ecb3ebfaf26574b0b3284fdced
Fayik <veznedaroglufayik2@gmail.com>
2022-10-09 16:43:51 +0300

    9.10.2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http"//import ediyoruz
-
+import { FormsModule } from '@angular/forms';
 import { CategoryComponent } from './component/category/category.component';
 import { NaviComponent } from './component/navi/navi.component';
 import { ProductComponent } from './component/product/product.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
 
 @NgModule({
   declarations: [//component kullanılacğı zaman otomatik olarak eklmee işlemini gerçekeleştirecektir. 
     AppComponent,
     CategoryComponent,
     NaviComponent,
-    ProductComponent
+    ProductComponent,
+    VatAddedPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule//hangi import elemanının kullanılcağoını belli ediyoruz
+    HttpClientModule,//hangi import elemanının kullanılcağoını belli ediyoruz
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -12,6 +12,9 @@ export class ProductComponent implements OnInit {
  
   products:Product[] = [];
   dataLoaded=false;
+  filterText="";
+
+
   constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
